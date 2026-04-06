54f5c3f688f03328ece6273e7911b8a727d1c564
ahmad-jueitem <ahmad.jueitem@Icloud.com>
2026-04-06 19:02:29 +0300

    fix pino-http import

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttp from "pino-http";
 import { IncomingMessage, ServerResponse } from "http";
 import router from "./routes";
 import { logger } from "./lib/logger";
@@ -8,7 +8,7 @@ import { logger } from "./lib/logger";
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  (pinoHttp as any)({
     logger,
     serializers: {
       req(req: IncomingMessage & { id?: string; url?: string; method?: string }) {
